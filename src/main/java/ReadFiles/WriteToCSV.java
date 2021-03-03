6cac185e9760e18b0cbdde7a638bc986a679fe4f
sidag99 <sidag991@gmail.com>
2021-03-03 10:48:05 +0530

    setup

--- src/main/java/ReadFiles/WriteToCSV.java
+++ src/main/java/ReadFiles/WriteToCSV.java
@@ -8,29 +8,30 @@ import java.io.IOException;
 
 public class WriteToCSV {
     // first create file object for file placed at location
-    // specified by filepath
-    private static File file;
-    // create FileWriter object with file as parameter
+    private File file;
 
-    public static void writeDataToCSV(String filePath) throws IOException {
-
+    public void initializeCsvWriter(String filePath) throws IOException {
         file = new File(filePath);
-        FileWriter outputFile = new FileWriter(file);
+    }
+    public void deleteContentsOfCsv(String filePath, String[] header) throws IOException {
+        file = new File(filePath);
+        if (file.exists() && file.isFile())
+        {
+            file.delete();
+        }
+        file.createNewFile();
+        FileWriter outputFile = new FileWriter(file, true);
         // create CSVWriter object fileWriter object as parameter
         CSVWriter writer = new CSVWriter(outputFile);
-
-        // adding header to csv
-        String[] header = {"Name", "Class", "Marks"};
         writer.writeNext(header);
-
-        // add data to csv
-        String[] data1 = {"Aman", "10", "620"};
-        writer.writeNext(data1);
-        String[] data2 = {"Suraj", "10", "630"};
-        writer.writeNext(data2);
-
-        // closing writer connection
         writer.close();
-
     }
+    public void writeCsv(String [] data) throws IOException {
+        FileWriter outputFile = new FileWriter(file, true);
+        // create CSVWriter object fileWriter object as parameter
+        CSVWriter writer = new CSVWriter(outputFile);
+        writer.writeNext(data);
+        writer.close();
+    }
+
 }
